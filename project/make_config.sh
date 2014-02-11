dff829782ce3a1450d1e63ee72439e16a2800346
alex <abondoa@gmail.com>
2014-02-11 18:43:45 +0100

    make config script

---
+++ project/make_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "<?php
+
+define('DB_HOST','127.0.0.1');
+define('DB_NAME','aaubordfodbold');
+define('DB_USER','root');
+define('DB_PASS','derpderpderp');
+
+define('PW_HASH','fUckd!1g@lG_^**^~d@r');" > config.php
